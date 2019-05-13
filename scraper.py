8fb8a9da27282ac993b2db48bb146eeee3d3572d
lernerbruno <bru.lerner@gmail.com>
2019-05-13 17:38:05 +0300

    Add yuri changes

--- scraper.py
+++ scraper.py
@@ -38,6 +38,19 @@ class Scraper:
         author = text_div.find('span', class_="authorOrTitle").text
         return author
 
+    def __get_likes(self, quote):
+        """Takes an individualized raw quote, and pulls out how many likes it got. Returns an integer."""
+        likes_banner = quote.find('div', class_='right')
+        likes = likes_banner.a.text
+        likes = int(likes.split()[0])
+        return likes
+
+    def __get_tags(self, quote):
+        """Takes an individualized raw quote and pulls out all of its tags. Returns them as a list of strings."""
+        tags_banner = quote.find('div', class_='quoteFooter')
+        tags_list = tags_banner.find_all('a')
+        print(tags_list)  # TODO: clean this string
+        
     def __new_url(self, url, page_num):
         """ Creates a proper URL containing the page number from the input and a
         default of showing 50 questions per page. """
