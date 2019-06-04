6f532c01e48406330c97975f881436b48b6204ef
ykk <yurikestenberg@hotmail.com>
2019-06-04 16:46:14 +0300

    Added argparser structure

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ Authors: Bruno Lerner, Doria Philo, Yuri Kaz"""
 from scraper import Scraper
 import outputter
 from config_file import ROOT_URL, OUTPUT_FILENAME
+import argparse
+
+
+def parsing_args():
+    """Organizes and parses arguments from the CLI."""
+
+    parser = argparse.ArgumentParser(description='Insert a description here later') # TODO describe the parser
+
+    parser.add_argument('-csv', action='store_true')
+
+    args = parser.parse_args()
+    return args
 
 
 def main():
+    # args = parsing_args() # TODO will return the arguments from CLI when function is done
     scraper_agent = Scraper(ROOT_URL)
     quotes_objects = scraper_agent.scrap()
     file_writer = outputter.Outputter(OUTPUT_FILENAME, 'csv', quotes_objects)
