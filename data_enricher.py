3866addce918aaed1693f3a2acda649715671ec7
doria <doriaph123@gmail.com>
2019-06-27 15:46:23 +0300

    adding the data enricher

--- data_enricher.py
+++ data_enricher.py
@@ -2,24 +2,66 @@
 Authors: Bruno Lerner, Doria Philo, Yuri Kaz"""
 
 import requests
-# 'action=parse&section=0&prop=text&page=pizza'
+import json
+from bs4 import BeautifulSoup
 
-class Data_Enricher:
-    """"""
-    def __init__(self, author):
-        self.root_url = r'http://en.wikipedia.org/w/api.php?'
-        self.author = author
-        self.text = self._get_page_content()
-        self.uris = []
 
-    def _set_page(self):
-        pass
+class DataEnricher:
+    """Enriches the data with Wikipedia information about the author. Return
+    the author's year of birth, gender, ethnic group and country of
+    nationality. """
 
-    def _get_page_content(self):
-        """Pulls the whole url's HTML as string."""
-        url = self.root_url + ''.join(self.uris)
-        r = requests.get()
-        print(r.text)
+    def __init__(self):
+        self.PROP_NAME = 0
+        self.PROP_NUM = 1
 
-Data_Enricher('Carlos')
+    def get_properties(self, author_name):
+        try:
+            data_id = self.get_data_id(author_name)
+        except KeyError:
+            return {}
+        properties = [('year', 'P569'),
+                      ('gender', 'P21'),
+                      ('ethnic_group', 'P172'),
+                      ('country', 'P27')]
+        author_props = {}
+        for prop in properties:
+            result = self.get_prop_info(data_id, prop)
+            author_props[prop[self.PROP_NAME]] = result
+        return author_props
 
+    def get_data_id(self, author_name):
+        ROOT_URL = r'https://en.wikipedia.org/w/api.php?action=query&prop=' \
+                   r'pageprops&ppprop=wikibase_item&redirects=1&format=' \
+                   r'json&titles='
+        full_url = ''.join((ROOT_URL, author_name))
+        raw = requests.get(full_url).text
+        info_dict = json.loads(raw)['query']['pages']
+        page_id = list(info_dict.keys())[0]
+        data_id = info_dict[page_id]['pageprops']['wikibase_item']
+        return data_id
+
+    def get_prop_info(self, data_id, prop):
+        ROOT_URL = r'https://www.wikidata.org/w/api.php?action=wbgetclaims' \
+                   r'&format=json&property={}&entity={}'
+        WIKIDATA_URL = 'https://www.wikidata.org/wiki/'
+        raw = requests.get(ROOT_URL.format(prop[self.PROP_NUM], data_id)).text
+        info_dict = json.loads(raw)['claims']
+        if info_dict == {}:
+            result = 'None'
+        if prop[self.PROP_NAME] == 'year':
+            birth_date = info_dict[prop[self.PROP_NUM]][0]["mainsnak"] \
+                ["datavalue"]["value"]["time"]
+            result = birth_date[1:5]
+        else:
+            wiki_data_id = info_dict[prop[self.PROP_NUM]][0]["mainsnak"] \
+                ["datavalue"]["value"]["id"]
+            html = requests.get(''.join((WIKIDATA_URL, wiki_data_id))).text
+            soup = BeautifulSoup(html, 'lxml')
+            result = soup.find('title').contents[0][:-11]
+        return result
+
+
+author = 'Edgar Allan Poe'
+enricher = DataEnricher()
+print(enricher.get_properties(author))
